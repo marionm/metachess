f2a98e050ca71bc1520b79c0813d3227e48e2bac
Mike Marion <mike.marion@gmail.com>
2012-04-15 17:04:03 -0500

    Fixing default game state

--- models/gameState.js
+++ models/gameState.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 var Schema = new mongoose.Schema({
-  state: { type: String, default: '7777777789abca98000000000000000000000000000000002345643211111111' },
+  state: { type: String, default: '89abca9877777777000000000000000000000000000000001111111123456432' },
   turn:  { type: Number, default: 0 }
 });
 
