800a5f11a40addc9cceea5a0ad40c640b4a65129
Mike Marion <mike.marion@gmail.com>
2013-05-05 14:28:00 -0500

    Remove extra logging statement

--- metachess.js
+++ metachess.js
@@ -9,5 +9,4 @@ require('./config/websockets.js')(io);
 
 var port = process.env.PORT || 3000;
 app.listen(port);
-console.log(port);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
